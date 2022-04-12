b44f532e0ce41eca9dc1a190acefe843f6374f8b
AlbertiPot <1071794833@qq.com>
2022-04-12 15:03:12 +0000

    add trainsearch on c100

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -42,156 +42,192 @@ parser.add_argument('--train_portion', type=float, default=0.5, help='portion of
 parser.add_argument('--unrolled', action='store_true', default=False, help='use one-step unrolled validation loss') # 二阶搜索unrolled，一阶不用
 parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
 parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
+parser.add_argument('--cifar100', action='store_true', default=False, help='search with cifar100 dataset')
 args = parser.parse_args()
 
 args.save = 'search-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
 utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
 
 log_format = '%(asctime)s %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format=log_format,
+                    datefmt='%m/%d %I:%M:%S %p')
 fh = logging.FileHandler(os.path.join(args.save, 'log.log'))
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
 
-
-CIFAR_CLASSES = 10
-
+if args.cifar100:
+    CIFAR_CLASSES = 100
+    data_folder = 'cifar-100-python'
+else:
+    CIFAR_CLASSES = 10
+    data_folder = 'cifar-10-batches-py'
 
 def main():
-  if not torch.cuda.is_available():
-    logging.info('no gpu device available')
-    sys.exit(1)
+    if not torch.cuda.is_available():
+        logging.info('no gpu device available')
+        sys.exit(1)
 
-  np.random.seed(args.seed)
-  torch.cuda.set_device(args.gpu)
-  cudnn.benchmark = True
-  torch.manual_seed(args.seed)
-  cudnn.enabled=True
-  torch.cuda.manual_seed(args.seed)
-  logging.info('gpu device = %d' % args.gpu)
-  logging.info("args = %s", args)
+    np.random.seed(args.seed)
+    torch.cuda.set_device(args.gpu)
+    cudnn.benchmark = True
+    torch.manual_seed(args.seed)
+    cudnn.enabled = True
+    torch.cuda.manual_seed(args.seed)
+    logging.info('gpu device = %d' % args.gpu)
+    logging.info("args = %s", args)
 
-  criterion = nn.CrossEntropyLoss()
-  criterion = criterion.cuda()
-  model = Network(args.init_channels, CIFAR_CLASSES, args.layers, criterion)
-  model = model.cuda()
-  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+    criterion = nn.CrossEntropyLoss()
+    criterion = criterion.cuda()
+    model = Network(args.init_channels, CIFAR_CLASSES, args.layers, criterion)
+    model = model.cuda()
+    logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
-  optimizer = torch.optim.SGD(
-      model.parameters(), ####### 这里指定用SGD对模型的权重训练
-      args.learning_rate,
-      momentum=args.momentum,
-      weight_decay=args.weight_decay)
+    optimizer = torch.optim.SGD(
+        model.parameters(),  ####### 这里指定用SGD对模型的权重训练
+        args.learning_rate,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay)
 
-  train_transform, valid_transform = utils._data_transforms_cifar10(args)
-  train_data = dset.CIFAR10(root=args.data, train=True, download=False, transform=train_transform)
+    #  prepare dataset
+    if args.cifar100:
+        train_transform, valid_transform = utils._data_transforms_cifar100(
+            args)
+    else:
+        train_transform, valid_transform = utils._data_transforms_cifar10(args)
 
-  num_train = len(train_data)
-  indices = list(range(num_train))
-  split = int(np.floor(args.train_portion * num_train)) # 用train set 训练参数时，根据train_portion划分训练集与验证集
+    if args.cifar100:
+        train_data = dset.CIFAR100(root=args.data,
+                                   train=True,
+                                   download=True,
+                                   transform=train_transform)
+    else:
+        train_data = dset.CIFAR10(root=args.data,
+                              train=True,
+                              download=False,
+                              transform=train_transform)
 
-  train_queue = torch.utils.data.DataLoader(
-      train_data, batch_size=args.batch_size,
-      sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[:split]),  # 训练集的指针 from 0 到split点
-      pin_memory=True) #, num_workers=2) 去掉才可能不broken pipe 32的错
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(args.train_portion * num_train))  # 用train set 训练参数时，根据train_portion划分训练集与验证集
 
-  valid_queue = torch.utils.data.DataLoader(
-      train_data, batch_size=args.batch_size,
-      sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]), # 验证集的指针 from split点 到数据集最后点
-      pin_memory=True) #, num_workers=2) 去掉才可能不报broken pipe 32的错
+    train_queue = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=args.batch_size,
+        sampler=torch.utils.data.sampler.SubsetRandomSampler(
+            indices[:split]),  # 训练集的指针 from 0 到split点
+        pin_memory=True)  #, num_workers=2) 去掉才可能不broken pipe 32的错
 
-  scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+    valid_queue = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=args.batch_size,
+        sampler=torch.utils.data.sampler.SubsetRandomSampler(
+            indices[split:num_train]),  # 验证集的指针 from split点 到数据集最后点
+        pin_memory=True)  #, num_workers=2) 去掉才可能不报broken pipe 32的错
+
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, float(args.epochs), eta_min=args.learning_rate_min)
 
-  architect = Architect(model, args)
+    architect = Architect(model, args)
 
-  for epoch in range(args.epochs):
-    lr = scheduler.get_last_lr()[0]
-    logging.info('epoch %d lr %e', epoch, lr)
+    for epoch in range(args.epochs):
+        lr = scheduler.get_last_lr()[0]
+        logging.info('epoch %d lr %e', epoch, lr)
 
-    genotype = model.genotype() # 读取当前epoch的结构参数并log保存，自己从log中读取保存的参数写入genotype中开始train_from the scratch
-    logging.info('genotype = %s', genotype)
+        genotype = model.genotype(
+        )  # 读取当前epoch的结构参数并log保存，自己从log中读取保存的参数写入genotype中开始train_from the scratch
+        logging.info('genotype = %s', genotype)
 
-    print(F.softmax(model.alphas_normal, dim=-1))
-    print(F.softmax(model.alphas_reduce, dim=-1))
+        print(F.softmax(model.alphas_normal, dim=-1))
+        print(F.softmax(model.alphas_reduce, dim=-1))
 
-    # training
-    train_acc, train_obj = train(train_queue, valid_queue, model, architect, criterion, optimizer, lr)
-    logging.info('train_acc %f', train_acc)
+        # training
+        train_acc, train_obj = train(train_queue, valid_queue, model,
+                                     architect, criterion, optimizer, lr)
+        logging.info('train_acc %f', train_acc)
 
-    # validation
-    valid_acc, valid_obj = infer(valid_queue, model, criterion)
-    logging.info('valid_acc %f', valid_acc)
+        # validation
+        valid_acc, valid_obj = infer(valid_queue, model, criterion)
+        logging.info('valid_acc %f', valid_acc)
 
-    utils.save(model, os.path.join(args.save, 'weights.pt'))
+        utils.save(model, os.path.join(args.save, 'weights.pt'))
 
-    scheduler.step()
+        scheduler.step()
 
 
-def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
-  objs = utils.AvgrageMeter()
-  top1 = utils.AvgrageMeter()
-  top5 = utils.AvgrageMeter()
+def train(train_queue, valid_queue, model, architect, criterion, optimizer,
+          lr):
+    objs = utils.AvgrageMeter()
+    top1 = utils.AvgrageMeter()
+    top5 = utils.AvgrageMeter()
 
-  for step, (input_, target) in enumerate(train_queue):      # 只训了1个epoch
-    model.train()
-    n = input_.size(0)
-    input_ = input_.cuda()
-    target = target.cuda(non_blocking=True)
+    for step, (input_, target) in enumerate(train_queue):  # 只训了1个epoch
+        model.train()
+        n = input_.size(0)
+        input_ = input_.cuda()
+        target = target.cuda(non_blocking=True)
 
-    # get a random minibatch from the search queue with replacement
-    input_search, target_search = next(iter(valid_queue))
-    input_search = input_search.cuda() # 输入image不需要跟踪grad，现在用torch.randn()生成的张量默认不跟踪grad，实例化conv等算子会调用parameter跟踪grad
-    target_search = target_search.cuda(non_blocking=True)
+        # get a random minibatch from the search queue with replacement
+        input_search, target_search = next(iter(valid_queue))
+        input_search = input_search.cuda(
+        )  # 输入image不需要跟踪grad，现在用torch.randn()生成的张量默认不跟踪grad，实例化conv等算子会调用parameter跟踪grad
+        target_search = target_search.cuda(non_blocking=True)
 
-    architect.step(input_, target, input_search, target_search, lr, optimizer, unrolled=args.unrolled)
+        architect.step(input_,
+                       target,
+                       input_search,
+                       target_search,
+                       lr,
+                       optimizer,
+                       unrolled=args.unrolled)
 
-    optimizer.zero_grad()
-    logits = model(input_)
-    loss = criterion(logits, target)
+        optimizer.zero_grad()
+        logits = model(input_)
+        loss = criterion(logits, target)
 
-    loss.backward()
-    nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
-    optimizer.step()
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+        optimizer.step()
 
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    objs.update(loss.data.item(), n)
-    top1.update(prec1.item(), n)
-    top5.update(prec5.item(), n)
+        prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+        objs.update(loss.data.item(), n)
+        top1.update(prec1.item(), n)
+        top5.update(prec5.item(), n)
 
-    if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+        if step % args.report_freq == 0:
+            logging.info('train %03d %e %f %f', step, objs.avg, top1.avg,
+                         top5.avg)
 
-  return top1.avg, objs.avg
+    return top1.avg, objs.avg
 
 
 def infer(valid_queue, model, criterion):
-  objs = utils.AvgrageMeter()
-  top1 = utils.AvgrageMeter()
-  top5 = utils.AvgrageMeter()
-  model.eval()
+    objs = utils.AvgrageMeter()
+    top1 = utils.AvgrageMeter()
+    top5 = utils.AvgrageMeter()
+    model.eval()
 
-  with torch.no_grad(): # 新增，降低内存使用
-    for step, (input_, target) in enumerate(valid_queue):
-      input_ = input_.cuda()
-      target = target.cuda(non_blocking=True)
+    with torch.no_grad():  # 新增，降低内存使用
+        for step, (input_, target) in enumerate(valid_queue):
+            input_ = input_.cuda()
+            target = target.cuda(non_blocking=True)
 
-      logits = model(input_)
-      loss = criterion(logits, target)
+            logits = model(input_)
+            loss = criterion(logits, target)
 
-      prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-      n = input_.size(0)
-      objs.update(loss.item(), n)
-      top1.update(prec1.item(), n)
-      top5.update(prec5.item(), n)
+            prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+            n = input_.size(0)
+            objs.update(loss.item(), n)
+            top1.update(prec1.item(), n)
+            top5.update(prec5.item(), n)
 
-      if step % args.report_freq == 0:
-        logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+            if step % args.report_freq == 0:
+                logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg,
+                             top5.avg)
 
-  return top1.avg, objs.avg
+    return top1.avg, objs.avg
 
 
 if __name__ == '__main__':
-  main() 
-
+    main()
